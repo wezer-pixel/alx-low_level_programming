c55ab3b1c508e1b12659f82f26d1ee7fb90ededd
wezer-pixel <jerryjuma104@gmail.cpm>
2023-03-17 09:36:02 +0000

    reduce wording

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- *main - Entry
+ *main -Entry
  *
- * Return: 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
