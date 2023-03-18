fe4ce4b453858b40e483517a01753430274b7c76
wezer-pixel <jerryjuma104@gmail.cpm>
2023-03-18 10:34:38 +0000

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets except e and q
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
