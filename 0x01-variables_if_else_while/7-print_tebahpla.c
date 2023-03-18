a2d1f71c7f5c701f9f0ae6fd0ff616a4cfa5dfe5
wezer-pixel <jerryjuma104@gmail.cpm>
2023-03-18 10:48:48 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lowecase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
