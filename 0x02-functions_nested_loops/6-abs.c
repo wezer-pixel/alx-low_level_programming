3b15cfe6556de50c28a39a3a40b903e1e805377e
wezer-pixel <jerryjuma104@gmail.cpm>
2023-03-21 10:57:53 +0000

    Return absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolut value of an int
+ * @n: number to be checked
+ *Return: abs of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
