6fb36bf23e01c7a1e16845ab2a44892eb3378176
wezer-pixel <jerryjuma104@gmail.cpm>
2023-03-27 15:02:59 +0000

    Cracker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,8 +3,7 @@
 #include <time.h>
 
 /**
- * main - Generates random valid passwords for the
- *        program 101-crackme.
+ * main - Generates random valid passwords
  * Return: Always 0.
  */
 int main(void)
@@ -13,7 +12,6 @@ int main(void)
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
@@ -50,5 +48,4 @@ int main(void)
 	printf("%s", password);
 
 	return (0);
-	A
 }
