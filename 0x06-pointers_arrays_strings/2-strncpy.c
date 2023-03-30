58c654d6d7673b30eed82ed8cbeb2c3b8f7d2dab
wezer-pixel <jerryjuma104@gmail.cpm>
2023-03-30 10:22:11 +0000

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @src: source of the string
+ * @dest: destination
+ * @n: length of the int
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	for (; i < n; i++)
+	{
+		*(dest +i) = '\0';
+	}
+	return (dest);
+}
