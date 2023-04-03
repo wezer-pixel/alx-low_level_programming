ca445ef93746c334f268d975c94b31e96b420928
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-03 10:31:47 +0000

    Function hat gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: segment
+ * @accept: prefix
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+	int a = 0, b, c;
+
+	for (b = 0; s[b] != '\0'; b++)
+	{
+		if (s[b] != 32)
+		{
+			for (c = 0; accept[c] != '\0'; c++;)
+			{
+				if (s[b] == accept[c])
+					a++;
+			}
+		}
+		else
+			return (a);
+	}
+	return (a);
+}
+
+
