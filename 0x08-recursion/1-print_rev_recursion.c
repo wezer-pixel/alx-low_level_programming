2c4ba2f6d26ff54ed496e1587268ac83bf8a7431
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-06 11:32:44 +0000

    Reverse it late

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string to check
+ * Return: void
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
