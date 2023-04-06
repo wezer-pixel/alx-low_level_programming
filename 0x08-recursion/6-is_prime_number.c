4fa464abe52c4baec208155cd9a7f676e6e3a88c
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-06 14:16:14 +0000

    Show me a true boolean if a number is prime onlr

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,30 @@
+
 #include "main.h"
+
 /**
- * is_prime_number - check if a number is a prime 
- * number
- * @n: number to check
- * Return: 0 or number
+ * check - check for prime number
+ * @a:int
+ * @b:int
+ * Return: bool
+ */
+int check(int a, int b)
+{
+	if (b < 2 || b % a == 0)
+		return (0);
+	else if (a > b / 2)
+		return (1);
+	else
+		return (check(a + 1, b));
+}
+
+/**
+ * is_prime_number - true if num is prime
+ * @n:int
+ * Return:int
  */
 int is_prime_number(int n)
 {
-	int divisor;
-
-	if (n < 2)
-	{
+	if (n == 2)
 		return (1);
-	}
-	else if (n % divisor == 0)
-	{
-		return (0);
-	}
-	else if (divisor * divisor > n)
-	{
-		return (1);
-	}
-	else
-	{
-		return (is_prime_number(n));
-	}
+	return (check(2, n));
 }
