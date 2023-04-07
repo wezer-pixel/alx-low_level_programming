766fe511b8e4978105356c2bb56e66e7e671e48d
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-07 02:14:30 +0000

    compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

---
+++ 0x08-recursion/101-wildcmo.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * str_checker - check if two strings identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index.
+ * Return: 1 if s is palindrome, else 0
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
+/**
+ * wildcmp - check if strings are identical
+ * @s1: base address for string.
+ * @s2: base address for string.
+ * Return: 1 if identical.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (str_checker(s1, s2, 0, 0));
+}
