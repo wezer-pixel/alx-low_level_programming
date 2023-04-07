2d8c7fd57d803b03aca2d6625d748da1a0a9077c
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-07 02:10:42 +0000

    Look out for palindromes

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- * _strlen_recursion - print length of string
- * is_palindrome - checks to see if string is
- * palindrome
- * @s: string
- * Return: int
+ * _strlen_recursion - print string length
+ * @s: the string to be printed
+ * Return: the length of string
  */
 int _strlen_recursion(char *s)
 {
 	if (s[0] != '\0')
-		return (1 + _strlen_recursion(s +1));
+		return (1 + _strlen_recursion(s + 1));
 	return (0);
 }
 /**
- * Second function
+ * pal_checker - check if string is palindrome.
+ * @s: string address.
+ * @i: left index.
+ * @j: rigth index.
+ * Return: 1 if s is palindrome, 0 otherwise.
  */
 int pal_checker(char *s, int i, int j)
 {
@@ -26,9 +28,11 @@ int pal_checker(char *s, int i, int j)
 		return (0);
 }
 /**
- * Third function
+ * is_palindrome - check if string is palindrome
+ * @s: base address
+ * Return: 1 if n is prime 0 if not
  */
 int is_palindrome(char *s)
 {
-	retuen (pal_checker(s, 0, _strlen_recursion(s) -1));
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
 }
