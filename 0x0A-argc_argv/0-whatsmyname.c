5d1f3917c17c54540f8d8050fa97f4e01513187c
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-09 06:23:04 +0000

    A program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	if (argc)
+	{
+	printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
