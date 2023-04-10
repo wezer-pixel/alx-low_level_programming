544b7724e5baacdff692cc06566c5bee6027edf3
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-10 09:53:42 +0000

    Stayed up all night just for a pirce of change - 21

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,8 +23,8 @@ int main(int argc, char **argv)
 
 	if (total < 0)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		//return (1);
 	}
 
 	for (i = 0; total > 0 && i < sizeof(cents) / sizeof(cents[0]); i++)
