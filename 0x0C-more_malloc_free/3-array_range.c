b5dfc04bdbdc2e56481a92ed17ec7b0d78dee2df
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-14 14:19:46 +0000

    More malloc tasks

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	size = max - min + 1;
-	ptr = malloc(sizeod(int) * size);
+	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
 		return (NULL);
