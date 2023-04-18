d6963a4bba3deb6cba52be1956cd95031d279b68
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-18 11:10:07 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,11 +7,17 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name == NULL)
+	{
 		d->name = "(nill)";
+	}
 	if (d->owner == NULL)
+	{
 		d->owner = "(nill)";
+	}
 	printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
