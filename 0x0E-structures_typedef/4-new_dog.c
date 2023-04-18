803c9a461f82a122b1ade9dca147a6d5e834a7b6
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-18 12:18:47 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,10 +7,12 @@
  */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (s[i] != '\0')
+	{
 		i++;
+	}
 	return (i);
 }
 
@@ -24,10 +26,16 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
+	len = 0;
+
 	while (src[len] != '\0')
+	{
 		len++;
+	}
 	for (i = 0; i < len; i++)
+	{
 		dest[i] = src[i];
+	}
 	dest[i] = '\0';
 
 	return (dest);
@@ -50,7 +58,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
 		return (NULL);
+	}
 
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
