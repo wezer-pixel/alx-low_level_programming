e4ce9543c71c777d9c4667741eb8aae0174a33fa
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-18 09:57:15 +0000

    C structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,26 @@
-struct dog{
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - A structure to show dog characteristic
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Dog owner
+ */
+struct dog
+{
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
