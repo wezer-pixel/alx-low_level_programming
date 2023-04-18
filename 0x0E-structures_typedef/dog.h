a79b91c4a537f043bc7e385c6cec4c01a9e9293c
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-18 12:09:10 +0000

    Create new dog function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,9 +18,9 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
-
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
-
+int _strlen(char *s);
 void free_dog(dog_t *d);
 
 #endif
