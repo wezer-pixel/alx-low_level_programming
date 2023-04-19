cbeb72649f070687862b5e82b9af233a9427201b
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-19 15:54:44 +0000

    Update header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*Prints a name*/
 void print_name(char *name, void (*f)(char *));
-
-/*executes a function given as a parameter on each element of an array*/
 void array_iterator(int *array, size_t size, void (*action)(int));
-
-/*searches for an integer*/
 int int_index(int *array, int size, int (*cmp)(int));
-
 int _putchar(char c);
 #endif /*FUNCTION_POINTERS_H*/
