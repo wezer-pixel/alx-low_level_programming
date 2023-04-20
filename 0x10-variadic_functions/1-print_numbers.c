315718cecff5117da337e2423e1f0810554344a2
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-20 12:34:34 +0000

    function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,22 +10,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	if (separator == NULL)
-	{
 		separator = "";
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
+		if (i < n - 1)
+			printf("%s", separator);
 	}
-	if (i < n - 1)
-	{
-		printf("%s", separator);
-	}
-
-	va_end(ap);
 	printf("\n");
+	va_end(ap);
 }
