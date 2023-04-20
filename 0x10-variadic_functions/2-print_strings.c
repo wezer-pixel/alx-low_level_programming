3dc989aa0a98c1f4b1d3227ffd60a457d5eeaf2c
wezer-pixel <jerryjuma104@gmail.cpm>
2023-04-20 13:56:57 +0000

    Print strings separated by separators

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,24 +9,24 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        char *str;
-        unsigned int i;
-        va_list ap;
+	char *str;
+	unsigned int i;
+	va_list ap;
 
-        va_start(ap, n);
-        if (separator == NULL)
-                separator = "";
+	va_start(ap, n);
+	if (separator == NULL)
+		separator = "";
 
-        for (i = 0; i < n; i++)
-        {
-                str = va_arg(ap, char*);
-                if (str == NULL)
-                        str = "(nil)";
-                printf("%s", str);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
 
-                if(i < n -1)
-                        printf("%s", separator);
-        }
-        printf("\n");
-        va_end(ap);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
 }
