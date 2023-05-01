e7f684b688f149449b22789e36e4eedbaf1912c5
wezer-pixel <jerryjuma104@gmail.com>
2023-05-01 17:59:18 +0000

    I'd like to meet with Tim Cook. I got some ideas

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_nodeint - add node at beginning of listint_t lst
+ * @head: head node
+ * @n: integer value to be stored in new code
+ * Return: NULL if fail
+ */
+listint *add_nodeint(listint_t **head, const int n)
+{
+	listint *new_node;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
