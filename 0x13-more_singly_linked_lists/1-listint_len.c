d6d114c1123823de4c7a0a9f309be7704fc834bb
wezer-pixel <jerryjuma104@gmail.com>
2023-05-01 17:33:12 +0000

    For me to say I wasn't a genius I'd just be lying to you and to myself

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 /**
- * listint_len - return no of element in a linked listint_t list
- * @head: head pointer
- * Return: no of elements in the list
+ * listint_len - No of elements in a linked listint_t list.
+ *@h: head pointer
+ * Return: the number of elements
  */
-size_t listint_len(const linstint_t *h)
+
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h)
 	{
-		h = h->next;
+	h = h->next;
 		count++;
 	}
 	return (count);
