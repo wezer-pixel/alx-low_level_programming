0cdf8aa71bb2c8b5cf076761d3d5730ddbc2171b
wezer-pixel <jerryjuma104@gmail.com>
2023-05-02 13:28:25 +0000

    I leave my emojis bart Simpson color

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,23 +6,20 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
+	listint_t *prev, *next;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
 	if ((*head)->next == NULL)
 		return (*head);
-
-	while (current != NULL)
+	prev = NULL;
+	while (*head != NULL)
 	{
-		listint_t *next = current->next;
-
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
-
 	*head = prev;
-	return (prev);
+	return (*head);
 }
