0929fd7b8e4ba7ab5eec82dbb0da81f6c02e3c96
wezer-pixel <jerryjuma104@gmail.com>
2023-05-02 12:00:31 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || idx == 0)
 	{
-		new_node->next = current;
+		new_node->next = current_node;
 		*head = new_node;
 		return (new_node);
 	}
