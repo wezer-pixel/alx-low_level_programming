b9de4ca6fa67c4b72c789739c7b88d32e7c7c6a4
wezer-pixel <jerryjuma104@gmail.com>
2023-05-02 12:19:42 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (current_node == NULL)
 			return (NULL);
 	}
-	new = malloc(sizeof(listint_t));
+	newi_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
