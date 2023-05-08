81c785eac35399d7eeabe66e80691aaa9d6fa5e3
wezer-pixel <jerryjuma104@gmail.com>
2023-05-08 22:44:07 +0000

    Burn those excel sheets

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename, O_RONLY);
+	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
