88c2a33b337e7d2b369df12732ae31f83f5c3a89
wezer-pixel <jerryjuma104@gmail.com>
2023-06-16 02:13:42 +0000

    Add node to end of a dll

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "list.h"
+
+/**
+ * add_dnodeint_end - add node to end of a dll
+ * @head: double ptr to begin of list
+ * @n: value to add to new node
+ *
+ * Return: pointer to new node, or NULL on failure
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *tmp;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
+}
